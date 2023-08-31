a1ad9759c1c81daa1595638c4fe3c1dce0793767
Valeria <valeria.vorontsova888@ya.ru>
2023-08-31 13:45:03 +0300

    изменения two

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -45,9 +45,9 @@ const requireDir = require("require-dir"),
             watch: "./src/img/sprites/*.svg"
         },
         fonts: {
-            src: "./src/fonts/**/*.{woff,woff2}",
+            src: "./src/fonts/**/*.{woff,woff2,ttf}",
             dist: "./dist/fonts/",
-            watch: "./src/fonts/**/*.{woff,woff2}"
+            watch: "./src/fonts/**/*.{woff,woff2,ttf}"
         },
         favicons: {
             src: "./src/img/favicon/*.{jpg,jpeg,png,gif}",
